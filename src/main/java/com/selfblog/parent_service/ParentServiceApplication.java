df9431902ad7bdc19b4b8d9da252ae37810f3d8d
短途游 <Link3750@aliyun.com>
2022-07-02 21:51:05 +0800

    Initial commit

---
+++ src/main/java/com/selfblog/parent_service/ParentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.selfblog.parent_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParentServiceApplication.class, args);
+    }
+
+}
